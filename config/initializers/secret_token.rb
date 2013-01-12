e5382447814cedafdcb4d15ed7904116f17047bb
Savannah Butler <savannahscarrone@gmail.com>
2013-01-12 15:09:36 -0500

    Started new library app project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibraryApp::Application.config.secret_token = '0759cc9c85b64427e9a0a15bef6dc5e4d2eb2c405ec62ed418505def572676bb8f7117de864f7bf99b08e46bc9e6344b348d74f8d0edd1ef74eaf4687b64f5bd'
